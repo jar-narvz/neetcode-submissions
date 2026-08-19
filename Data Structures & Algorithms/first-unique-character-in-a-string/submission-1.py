e62bdaa6b661c5b7b17e8fb640dd40a2247901a6
jar-narvz <jarjar0623@gmail.com>
2026-08-18 17:52:26 -0700

    Add: first-unique-character-in-a-string - submission-1

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = {}
+
+        for c in s:
+            count[c] = count.get(c, 0) + 1
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
+        return -1
